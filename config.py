65d2dd0c6a92422ef8f6dcefc6e7a634de8054e0
Kang Wang <kw600@cam.ac.uk>
2023-04-17 00:48:46 +0100

    fix

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ k_spacing = 0.2 #A^-1 The minimum distance in the Brillouin zone sampling
 
 # configurations for DFT calculations
 #number of configurations. Better to be a power of 64
-N_config = 512
+N_config = 256
 maxpop=5 #maximum population/iteration
 
 
