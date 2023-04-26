7b6a3fb179ff77dc7311063d4a9bbeca45de559b
Kang Wang <kw600@cam.ac.uk>
2023-04-26 20:27:11 +0100

    remove auto

--- config.py
+++ config.py
@@ -1,17 +1,18 @@
 import os
 
 #archer2 details. Please change the account name!!
-taskname = 'scha1' #name of the task. Should be different if several auto_run.py are running at the same time 
+taskname = 'scha3' #name of the task. Should be different if several auto_run.py are running at the same time 
 account = 'e89-ic_m' #account name
 #number of tasks per node. Please leave a space between '=' and number.
-n_node_per_job = 1
-nrun_per_node = 32
-hour = '02'
+n_node_per_job = 4
+nrun_per_node = 4
+hour = '04'
 one_by_one = True
 
 
 #general
-nq1 = 3 #supercell size in each direction
+#supercell size in each direction
+nq1 = 3 
 nq2 = 3
 nq3 = 3
 T0 = 300
@@ -29,9 +30,9 @@ nqirr = cq - 1 #number of irreducible q points
 
 # configurations for DFT calculations
 #number of configurations. Better to be a power of 64
-N_config = 1024
+N_config = 256
 #maximum population/iteration
-maxpop = 20
+maxpop = 5
 
 
 #step2 DFT calculations for the ensemble of configurations
@@ -41,4 +42,8 @@ conv_thr_2 = 1e-8
 
 #minimization
 #0.1 for the first few ensembles (with large FC gradients) and 0.5 for the later ones
-kong_liu_ratio = 0.2
+if os.path.isdir('run_dft1'):
+    kong_liu_ratio = 0.5
+else: 
+    kong_liu_ratio = 0.2
+
