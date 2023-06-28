e2e8577d8bcb9dc3372076f8bf244025f497d0e9
Kang Wang <kw600@cam.ac.uk>
2023-06-28 15:52:51 +0100

    qe2qe

--- qe2qe.py
+++ qe2qe.py
@@ -7,19 +7,20 @@ This script read the QE dynamical matrix and use q2r.x to get the force constant
 import os,shutil
 
 input_file = 'dyn_pop2_'
+output_dir = 'lte_881'
 output_file = 'qe2qe_881_dyn'
 
 # generate delta_prim.dat using generate_R.py
-shutil.copy(input_file+'1', "harmonic1")
-shutil.copy(input_file+'0', "harmonic0")
-os.system("python generate_R.py ./")
+shutil.copy(input_file+'1', output_dir+"harmonic1")
+shutil.copy(input_file+'0', output_dir+"harmonic0")
+os.system(f"python generate_R.py {output_dir}")
 
 # use q2r.x to get the force constant matrix
 q2r=f'''
 &input
     fildyn = '{input_file}'
     zasr = 'simple'
-    flfrc = 'format2.txt'
+    flfrc = '{output_dir}/format2.txt'
 /
 '''
 with open('q2r.in', 'w') as f:
@@ -27,11 +28,11 @@ with open('q2r.in', 'w') as f:
 os.system("q2r.x < q2r.in > q2r.out")
 
 # transform the force constant matrix into force.dat
-os.system("python qe_to_f90force.py ./")
+os.system(f"python qe_to_f90force.py {output_dir}")
 
 # generate the final QE matrix with different supercell size
 # target supercell size needs to be consistent the harmonic_xxx_dyn files in the directory
-os.system("python farey2qe.py ./")
+os.system(f"python farey2qe.py {output_dir}")
 
 
 
