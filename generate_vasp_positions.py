13890475d8d7b2bf55744915012b58dfb46da81c
Kang Wang <kw600@cam.ac.uk>
2023-07-17 10:28:41 +0100

    fix

--- generate_vasp_positions.py
+++ generate_vasp_positions.py
@@ -1,16 +1,17 @@
 import numpy as np
 import sys
-N_config = 512
-N_prim=13
-N_supercell=3*3*3
+import config
+N_config = config.N_config
+N_prim=4
+N_supercell=2*2*2
 pop=sys.argv[1]
-f=open(f'./energies_supercell_population{pop}.dat','w');f.close()
+f=open(f'./ens{pop}/energies_supercell_population{pop}.dat','w');f.close()
 for i in range(1,N_config+1):
     if i % 50 == 0:
         print(f'Reading configuration {i}')
     #get the force and stress
     stress=np.zeros((3,3))
-    with open(f'../run_dft{pop}/vasp{i}/OUTCAR') as f0:
+    with open(f'./run_dft{pop}/vasp{i}/OUTCAR') as f0:
         d=f0.readlines()
     for ii in range(len(d)):
         if 'TOTAL-FORCE' in d[ii]:
@@ -23,57 +24,29 @@ for i in range(1,N_config+1):
     force=d1[:,3:]/25.71104309541616
     stress=stress/(29421.02648438959/2*10)
 
-    #get the total energy
-
-    # with open(f'../run_dft{pop}/vasp{i}/OSZICAR') as ff:
-    #     dd=ff.readlines()[-1]
-    # energy=float(dd.split()[4])/(27.211396/2)
- 
-            #d is outcar readlines
     for jj in d:
         if 'average (electrostatic) potential at core' in jj:
             break
         elif 'energy(sigma->0)' in jj:
             energy=float(jj.split()[-1])/(27.211396/2)
 
-
-
-
-    # write the position. Only used for test
-    # sc=pos[:1*N_supercell];v=pos[1*N_supercell:7*N_supercell];sn=pos[7*N_supercell:13*N_supercell]
-    # sc=[i for i in sc];v=[i for i in v];sn=[i for i in sn]
-    # with open(f'./vasp_population{pop}_{i}.dat','w') as f1:
-    #     f1.writelines(L)
-    #     for j in range(N_supercell):
-    #         a=sc.pop(0)
-    #         f1.write(f'Sc {a[0]} {a[1]} {a[2]}\n')
-    #         for k in range(6):
-    #             a=v.pop(0)
-    #             f1.write(f'V {a[0]} {a[1]} {a[2]}\n')
-    #         for k in range(6):
-    #             a=sn.pop(0)
-    #             f1.write(f'Sn {a[0]} {a[1]} {a[2]}\n')
-
     #write the force
-    sc=force[:1*N_supercell];v=force[1*N_supercell:7*N_supercell];sn=force[7*N_supercell:13*N_supercell]
-    sc=[i for i in sc];v=[i for i in v];sn=[i for i in sn]
-    with open(f'./forces_population{pop}_{i}.dat','w') as f2:
+    sc=force[:4*N_supercell]
+    sc=[i for i in sc]
+    with open(f'./ens{pop}/forces_population{pop}_{i}.dat','w') as f2:
         for j in range(N_supercell):
-            a=sc.pop(0)
-            f2.write(f' {a[0]} {a[1]} {a[2]}\n')
-            for k in range(6):
-                a=v.pop(0)
-                f2.write(f' {a[0]} {a[1]} {a[2]}\n')
-            for k in range(6):
-                a=sn.pop(0)
+            for k in range(4):
+                a=sc.pop(0)
                 f2.write(f' {a[0]} {a[1]} {a[2]}\n')
     #write the stress
-    with open(f'./pressures_population{pop}_{i}.dat','w') as f3:
+    with open(f'./ens{pop}/pressures_population{pop}_{i}.dat','w') as f3:
         for k in range(3):
             f3.writelines(f'{stress[k,0]} {stress[k,1]} {stress[k,2]}\n')
     #write the total energy
-    with open(f'./energies_supercell_population{pop}.dat','a') as f4:
+    with open(f'./ens{pop}/energies_supercell_population{pop}.dat','a') as f4:
         f4.write(f'{energy}\n')
+
+
 
 
 
